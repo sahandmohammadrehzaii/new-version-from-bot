79cfc1e8a86212ad93dac8c3ac69e572a139f03c
sahandmohammadrezaii <139042771+sahandmohammadrehzaii@users.noreply.github.com>
2023-07-28 20:46:45 +0330

    Add files via upload

---
+++ نسخه مود(فارسی) ربات هوا شناسی.py
@@ -0,0 +1,39 @@
+# Information Robot
+for i in range(1):
+   print(" سلام ")
+for i in range(1):
+   print(" هستم ربات يک من ")
+for i in range(1):
+   print("است استراتوس من نام ")
+for i in range(1):
+   print("هستم هواشناسي ربات يک من ")
+for i in range(1):
+   print("بپوشيد بايد را هايي لباس چه موقيعت هر در که بگويم شما به و بنويسم را هوا و آب نوع و بگيرم شما از را دما که است اين من وظيفه ")
+for i in range(1):
+   print("است ابر يک نام استراتوس چون ")
+# Information Ask Weather
+x=input("بنويسيد را دما مقدار توانيد مي شما :")
+x=float(x)
+# Information Temperature value
+# Information Weather Cold
+if x<30:
+   print("است سرد هوا ")
+   print("بخوريد گرم غذاي و چاي و بپوشيد گرم لباس که کنم مي توصيه ")
+   print("برويد دکتر به که کنم مي توصيه ")
+   print("نشويد مريض که باشيد مراقب نه اگر و کنيد استراحت  بايد خورديد سرما اگر ")
+   print("بپرهيزيد سردي مواد خوردن از ")
+# Information Weather Hot
+if x>30:
+   print(" است گرم هوا")
+   print("بنوشيد بيشتري مايعات و بپوشيد نازک هاي لباس که کنم مي توصيه ")
+   print(" ببريد آب خودتان با رويد مي بيرون وقتي پس باشيد داشته بدي احساس و شويد زدگي گرما دچار ممکن رويد مي بيرون گرما در اگر ")
+# Information Weather Mild
+if x<30 and x>30:
+   print("است ملايم هوا ")
+   print(" ندارم اي توصيه هيچ ")
+   print("باشيد خودتان سلامتي مراقب ")
+# Information finish work
+for i in range(1):
+   print(" خداحافظ ")
+for i in range(1):
+   print("Hello sahand mohammad rezaiee")
